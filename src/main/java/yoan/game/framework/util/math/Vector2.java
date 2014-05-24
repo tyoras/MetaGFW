ef183c5ebc10571a65648cba14dbbb78f0af6039
Yoan <tyoras@gmail.com>
2014-05-24 14:20:55 +0200

    adding tests to Vector2

--- src/main/java/yoan/game/framework/util/math/Vector2.java
+++ src/main/java/yoan/game/framework/util/math/Vector2.java
@@ -218,4 +218,40 @@ public class Vector2 {
 		float distY= this.y - y;
 		return distX * distX + distY * distY;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode(){
+		final int prime= 31;
+		int result= 1;
+		result= prime * result + Float.floatToIntBits(x);
+		result= prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj){
+		if(this == obj) return true;
+		if(obj == null) return false;
+		if(getClass() != obj.getClass()) return false;
+		Vector2 other= (Vector2) obj;
+		if(Float.floatToIntBits(x) != Float.floatToIntBits(other.x)) return false;
+		if(Float.floatToIntBits(y) != Float.floatToIntBits(other.y)) return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString(){
+		return "Vector2 [" + x + ", " + y + "]";
+	}
+	
+	
 }
--- src/test/java/yoan/game/framework/util/math/region/Vector2Test.java
+++ src/test/java/yoan/game/framework/util/math/region/Vector2Test.java
@@ -22,6 +22,20 @@ public class Vector2Test {
 	}
 	
 	@Test
+	public void add_vector_should_work() {
+		//given
+		Vector2 tested = new Vector2(2, 3);
+		Vector2	added = new Vector2(3, 8);	
+		
+		//when
+		tested.add(added);
+		
+		//then
+		assertThat(tested.x).isEqualTo(5);
+		assertThat(tested.y).isEqualTo(11);
+	}
+	
+	@Test
 	public void sub_should_work() {
 		//given
 		Vector2 tested = new Vector2(2, 3);
@@ -32,5 +46,119 @@ public class Vector2Test {
 		//then
 		assertThat(tested.x).isEqualTo(-1);
 		assertThat(tested.y).isEqualTo(-5);
+	}
+	
+	@Test
+	public void sub_vector_should_work() {
+		//given
+		Vector2 tested = new Vector2(2, 3);
+		Vector2	subed = new Vector2(3, 8);	
+		
+		//when
+		tested.sub(subed);
+		
+		//then
+		assertThat(tested.x).isEqualTo(-1);
+		assertThat(tested.y).isEqualTo(-5);
+	}
+	
+	@Test
+	public void set_should_work() {
+		//given
+		Vector2 tested = new Vector2(2, 3);
+		
+		//when
+		tested.set(3, 8);
+		
+		//then
+		assertThat(tested.x).isEqualTo(3);
+		assertThat(tested.y).isEqualTo(8);
+	}
+	
+	@Test
+	public void cpy_should_work() {
+		//given
+		Vector2 origin = new Vector2(2, 3);
+		
+		//when
+		Vector2 copy = origin.cpy();
+		
+		//then
+		assertThat(copy == origin).isFalse();
+		assertThat(copy.equals(origin)).isTrue();
+		assertThat(copy.x).isEqualTo(origin.x);
+		assertThat(copy.y).isEqualTo(origin.y);
+	}
+	
+	@Test
+	public void mul_should_work() {
+		//given
+		Vector2 tested = new Vector2(2, 3);
+		
+		//when
+		tested.mul(3);
+		
+		//then
+		assertThat(tested.x).isEqualTo(6);
+		assertThat(tested.y).isEqualTo(9);
+	}
+	
+	@Test
+	public void len_should_work() {
+		//given
+		Vector2 tested = new Vector2(4, 3);
+		
+		//when
+		float length = tested.len();
+		
+		//then
+		assertThat(length).isEqualTo(5);
+	}
+	
+	@Test
+	public void nor_should_work() {
+		//given
+		Vector2 tested = new Vector2(4, 3);
+		
+		//when
+		tested.nor();
+		
+		//then
+		assertThat(tested.x).isEqualTo(0.8f);
+		assertThat(tested.y).isEqualTo(0.6f);
+	}
+	
+	@Test
+	public void angle_should_work() {
+		//given
+		Vector2 tested1 = new Vector2(0, 0); Vector2 tested2 = new Vector2(1, 1);
+		Vector2 tested3 = new Vector2(0, 1); Vector2 tested4 = new Vector2(-1, 1);
+		Vector2 tested5 = new Vector2(-1, 0); Vector2 tested6 = new Vector2(-1, -1);
+		Vector2 tested7 = new Vector2(0, -1); Vector2 tested8 = new Vector2(1, -1);
+		
+		//when
+		float angle1 = tested1.angle(); float angle2 = tested2.angle(); 
+		float angle3 = tested3.angle(); float angle4 = tested4.angle();
+		float angle5 = tested5.angle(); float angle6 = tested6.angle();
+		float angle7 = tested7.angle(); float angle8 = tested8.angle();
+		
+		//then
+		assertThat(angle1).isEqualTo(0); assertThat(angle2).isEqualTo(45);
+		assertThat(angle3).isEqualTo(90); assertThat(angle4).isEqualTo(135);
+		assertThat(angle5).isEqualTo(180); assertThat(angle6).isEqualTo(225);
+		assertThat(angle7).isEqualTo(270); assertThat(angle8).isEqualTo(315);
+	}
+	
+	@Test
+	public void rotate_should_work() {
+		//given
+		Vector2 tested = new Vector2(1, 0);
+		
+		//when
+		tested.rotate(90);
+
+		//then
+		float dist = tested.dist(new Vector2(0, 1));
+		assertThat(dist >= 0 && dist <= 0.000001).isTrue(); 
 	}
 }
